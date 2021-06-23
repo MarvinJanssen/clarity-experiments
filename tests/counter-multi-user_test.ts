2b95449f94ea106f8b01a9931ad8f7828e7fb301
Ludo Galabru <ludovic@blockstack.com>
2021-06-23 19:05:51 -0400

    chore: update imports

--- tests/counter-multi-user_test.ts
+++ tests/counter-multi-user_test.ts
@@ -1,4 +1,4 @@
-import { Clarinet, Tx, Chain, Account } from 'https://deno.land/x/clarinet@v0.10.0/index.ts';
+import { Clarinet, Tx, Chain, Account } from 'https://deno.land/x/clarinet@v0.13.0/index.ts';
 
 Clarinet.test({
 	name: "Anyone can count up their counter",
